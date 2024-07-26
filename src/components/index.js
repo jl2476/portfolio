c6001dfe2948b2817951487740780e639cc901b7
jl2476 <123974333+jl2476@users.noreply.github.com>
2024-07-26 17:21:46 -0400

    Updated index.js
    Was breaking compilation

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Contact from "./Contact";
 import Links from './Links';
-import Resume from './resume';
+import Resume from './Resume';
 import Experience from "./Experience";
 import Hero from "./Hero";
 import CanvasLoader from "./Loader";
